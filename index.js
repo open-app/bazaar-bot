b7ffd9f1c309ef53eff1d6f3bfccfe106067fe1f
luandro <luandro@gmail.com>
2019-02-08 17:24:46 -0200

    fixed plugin local copy

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const server = require('open-app-graphql-server')
 const ssb = require('ssb-graphql-defaults')
 const economic = require('economic-sentences-graphql')
-const bazaar = require('../open-app/bazaar-graphql')
+const bazaar = require('bazaar-graphql')
 require('./bot')
 server([
   ssb,
