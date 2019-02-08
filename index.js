18989725bba1d6f016c4bae91a3de0caab63b032
luandro <luandro@gmail.com>
2019-02-08 17:21:35 -0200

    fixed hardcoded scope

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const server = require('open-app-graphql-server')
 const ssb = require('ssb-graphql-defaults')
 const economic = require('economic-sentences-graphql')
-const bazaar = require('bazaar-graphql')
+const bazaar = require('../open-app/bazaar-graphql')
 require('./bot')
 server([
   ssb,
   economic,
   bazaar,
 ], {
-  scope: 'ssb-01Sk9Yyu',
+  scope: process.env.SCOPE,
   key: process.env.APP_KEY,
 })
