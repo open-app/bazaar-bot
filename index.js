0d9c5917209ad6874393fa09d8ca144107e61afa
luandro <luandro@gmail.com>
2020-04-06 18:09:32 -0300

    removed open-app-graphql-server and added separate packages

--- index.js
+++ index.js
@@ -1,13 +1,2 @@
-const server = require('open-app-graphql-server')
-const ssb = require('ssb-graphql-defaults')
-const economic = require('economic-sentences-graphql')
-const bazaar = require('bazaar-graphql')
 require('./bot')
-server([
-  ssb,
-  economic,
-  bazaar,
-], {
-  scope: process.env.SCOPE,
-  key: process.env.APP_KEY,
-})
+require('./server/ssb')
