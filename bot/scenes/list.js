8342939c05a82e6073e1c7091cb64e520a180421
luandro <luandro@gmail.com>
2020-04-11 17:03:39 -0300

    Signal donation instead of 0 on list

--- bot/scenes/list.js
+++ bot/scenes/list.js
@@ -3,17 +3,27 @@ const { getPublishedResources } = require('../api')
 const i18n = require('../lib/localization')
 
 const list = new Scene('list')
-list.enter((async ctx => {
+list.enter(async ctx => {
   const data = await getPublishedResources()
-  const list = data.map(((item) => {
-    const { index, user, category, price} = item
+  const list = data.map(item => {
+    let { index, user, category, price } = item
+    if (
+      price.split('&').length === 1 &&
+      parseInt(price.split(process.env.COMMUNITY_CURRENCY)[0]) === 0
+    ) {
+      price = i18n(ctx, 'newOpt1')
+    }
     return `\`${index + 1}\` - @${user}: \`${category}\` ${price}`
-  }))
+  })
   if (list.length > 0) {
-    await ctx.replyWithMarkdown(list.join('\n') + '\n' + i18n(ctx, 'resourcesAdd'))
+    await ctx.replyWithMarkdown(
+      list.join('\n') + '\n' + i18n(ctx, 'resourcesAdd')
+    )
   } else {
-    await ctx.replyWithMarkdown(`${i18n(ctx, 'empty')}.\n${i18n(ctx, 'resourcesAdd')}`)
+    await ctx.replyWithMarkdown(
+      `${i18n(ctx, 'empty')}.\n${i18n(ctx, 'resourcesAdd')}`
+    )
   }
   return ctx.scene.leave()
-}))
+})
 module.exports = list
